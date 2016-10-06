20ffd66a9583f9add59cc808da114fac1f9a6f26
Andy Armstrong <andy@hexten.net>
2016-10-06 17:11:40 +0100

    WIP controller.

---
+++ lib/Fenchurch/Syncotron/Controller.pm
@@ -0,0 +1,16 @@
+package Fenchurch::Syncotron::Controller;
+
+our $VERSION = "0.01";
+
+use Moose;
+use MooseX::Storage;
+
+=head1 NAME
+
+Fenchurch::Syncotron::Controller - Sync protocol controller
+
+=cut
+
+1;
+
+# vim:ts=2:sw=2:sts=2:et:ft=perl
