8abaaa014cd7ba73abd092fc1e58bb418cee8a79
Andy Armstrong <andy@hexten.net>
2017-01-07 16:35:23 +0000

    Force DBD-mysql 4.038

---
+++ bin/dbi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Temp helper to force the right DBD-mysql version
+
+cpanm http://search.cpan.org/CPAN/authors/id/C/CA/CAPTTOFU/DBD-mysql-4.038.tar.gz
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
